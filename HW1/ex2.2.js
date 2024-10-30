35784d968203db3a9f590630b15055a66875ca4a
undercurrent1962 <undercurrent@gmail.com>
2024-10-30 14:39:29 +0900

    HW1_modified

--- HW1/ex2.2.js
+++ HW1/ex2.2.js
@@ -2,22 +2,6 @@ const path = require("path");
 const fs = require("fs");
 const util = require("util");
 
-
-// const findJS = fileToRead => {
-//     fs.readdir(fileToRead, (err, files) => {
-//         files.forEach((file) => { // use forEach instead of for of loop
-//             fs.stat(fileToRead + "\\" + file, (err, cur) => {
-//                 if (cur.isDirectory()) {
-//                     findJS(fileToRead + "\\" + file);
-//                 } else if (file.endsWith(".js")) {
-//                     console.log(fileToRead + "\\" + file);
-//                 }
-//             });
-//         });
-//     })
-// };
-// findJS("./test");
-
 const getDirList = util.promisify(fs.readdir);
 const getFileStat = util.promisify(fs.stat);
 const PATH = './test';
@@ -27,7 +11,7 @@ const searchDirectory = async directory => {
     files.forEach(async file => {
         const filePath = path.join(directory, file);
         const stat = await getFileStat(filePath);
-        
+
         if (stat.isDirectory()) await searchDirectory(filePath);
         else if (path.extname(filePath) === '.js') console.log(filePath);
     });
