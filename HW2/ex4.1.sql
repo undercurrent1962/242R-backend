b021f83e2ea323aaee7663f6324cfd447a7eab1b
undercurrent1962 <undercurrent@gmail.com>
2024-10-31 13:55:35 +0900

    HW2_modified

--- HW2/ex4.1.sql
+++ HW2/ex4.1.sql
@@ -1,4 +1,5 @@
 CREATE TABLE `students`(
+    `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(20) NOT NULL,
     `enrollment_year` INT NOT NULL,
     `department` VARCHAR(30) NOT NULL,
@@ -7,5 +8,7 @@ CREATE TABLE `students`(
     `address` VARCHAR(50) NOT NULL,
     `total_credit` INT DEFAULT 0,
     `GPA` FLOAT DEFAULT 0.0,
-    `in_school` BOOLEAN DEFAULT TRUE
-)
+    `in_school` BOOLEAN DEFAULT TRUE,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
